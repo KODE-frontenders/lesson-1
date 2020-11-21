706c291760b8d13ab2bebd4a2e61baecd4e9718a
Martyn <zogacc@gmail.com>
2020-11-21 11:21:34 +0200

    fix global styles theming

--- src/components/atoms/global-styles/global-styles.jsx
+++ src/components/atoms/global-styles/global-styles.jsx
@@ -3,8 +3,8 @@ import "fontsource-roboto";
 
 export const GlobalStyles = createGlobalStyle`
   body {
-    background: ${({ theme }) => theme.background};
-    color: ${({ theme }) => theme.text};
+    background: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors.text};
     font-family: Roboto, sans-serif;
     transition: all 0.50s linear;
   }
