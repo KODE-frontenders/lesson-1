3be926042105a8d20cf74b6d1beca4a92b1c6ba7
Martyn <zogacc@gmail.com>
2020-11-21 11:21:02 +0200

    extend theme

--- src/theme.js
+++ src/theme.js
@@ -1,25 +1,45 @@
-export const colors = {
+const colors = {
   blueColor: "#006CFF",
   dimmedBlueColor: "#B2D2FF",
-  whiteColor: "#FFFFFF",
+  whiteColor: "#FFF",
   lightGrayColor: "#DADADA",
-  grayColor: "#777777",
-  darkGrayColor: "#333333",
+  grayColor: "#777",
+  darkGrayColor: "#333",
+  trasparentBlackColor: "#0005",
+
 };
 
+const sizeGrid = {
+  /** eight-point size grid */
+  half: 4,
+  base: 8,
+  double: 16,
+  triple: 24,
+  quadruple: 32,
+  tenfold: 80,
+  hundredfold: 800,
+};
 
 export const lightTheme = {
-         text: colors.darkGrayColor,
-         background: colors.whiteColor,
-         primary: colors.blueColor,
-         secondary: colors.lightGrayColor,
-         colors,
-       };
+                colors: {
+                  text: colors.darkGrayColor,
+                  background: colors.whiteColor,
+                  primary: colors.blueColor,
+                  secondary: colors.lightGrayColor,
+                  shadow: colors.trasparentBlackColor,
+                  colors,
+                },
+                sizeGrid,
+              };
 
 export const darkTheme = {
-         text: colors.whiteColor,
-         background: colors.darkGrayColor,
-         primary: colors.dimmedBlueColor,
-         secondary: colors.grayColor,
-         colors,
-       };
+                colors: {
+                  text: colors.whiteColor,
+                  background: colors.darkGrayColor,
+                  primary: colors.dimmedBlueColor,
+                  secondary: colors.grayColor,
+                  shadow: colors.trasparentBlackColor,
+                  colors,
+                },
+                sizeGrid,
+              };
