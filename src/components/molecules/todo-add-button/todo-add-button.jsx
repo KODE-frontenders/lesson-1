86224d82b29438749da10364355d8f232674020e
Martyn <zogacc@gmail.com>
2020-11-21 09:59:10 +0200

    add styling for add button component

--- src/components/molecules/todo-add-button/todo-add-button.jsx
+++ src/components/molecules/todo-add-button/todo-add-button.jsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { ReactComponent as PlusIcon } from "icons/plus-icon.svg";
+import { ReactComponent as PlusIcon } from "shared/icons/plus-icon.svg";
+import styled, { ThemeContext } from "styled-components";
+
+const StyledButton = styled.button`
+  line-height: 1;
+  padding: ${(props) => props.size}px;
+  margin: ${(props) => props.size}px;
+  border-radius: 50%;
+  border: none;
+  box-shadow: 0px 24px 32px ${(props) => props.theme.colors.darkGray};
+`;
 
 export const TodoAddButton = ({ onToggle }) => {
+  const theme = React.useContext(ThemeContext);
+
   return (
-    <button type="button" onClick={onToggle}>
+    <StyledButton type="button" theme={theme} size={16} onClick={onToggle}>
       <PlusIcon />
-    </button>
+    </StyledButton>
   );
 };
