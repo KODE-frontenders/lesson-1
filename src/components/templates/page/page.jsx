bc8a04a6e46a8b4c7dc0e6f6afa9e12e4ef14049
Martyn <zogacc@gmail.com>
2020-11-22 18:02:04 +0200

    add page template

---
+++ src/components/templates/page/page.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+import { Header } from "components/templates/header/header";
+import { Body } from "components/templates/body/body";
+
+const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  overflow-y: hidden;
+  width: 100%;
+  padding: ${({ theme }) => theme.sizeGrid.double}px;
+`;
+
+export const PageTemplate = ({ children, title, linkTo }) => {
+  return (
+    <PageWrapper>
+      <Header title={title} linkTo={linkTo} />
+      <Body>{children}</Body>
+    </PageWrapper>
+  );
+};
